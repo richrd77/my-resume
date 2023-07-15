a3935b92f1b6ec58e7d2238414691e087ed83216
richrd77@gmail.com <richrd77@gmail.com>
2023-07-15 13:27:13 +0530

    build

--- src/app/components/index.ts
+++
@@ -1,6 +0,0 @@
-import { FooterComponent } from "./footer/footer.component";
-import { HeaderComponent } from "./header/header.component";
-import { HolderComponent } from "./holder/holder.component";
-import { PersonalComponent } from "./personal/personal.component";
-
-export { HeaderComponent, FooterComponent, HolderComponent, PersonalComponent };
